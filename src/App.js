7081f35b7e1de5c7b7a13eb9b26c718ffe5d83de
Mokhanko <co41986@gmail.com>
2019-03-12 16:31:33 +0200

    socket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, {Fragment} from 'react';
-import { compose, withState, lifecycle } from 'recompose';
+import { compose, lifecycle, withState, withHandlers } from 'recompose';
+import openSocket from 'socket.io-client';
 
-const App = () => (
+const socketConnection = openSocket('http://localhost:3001/');
+
+const App = ({ username, usersOnline, changeUsername, login }) => (
   <Fragment>
     <h1>Realtime communication with WebRTC</h1>
+    <label htmlFor="usernameInput">Login</label>
+    <input type="text" id="usernameInput" placeholder="Login" onChange={(e) => changeUsername(e.target.value)}/>
+      <button id="loginBtn" onClick={() => login()}>
+        Sign in
+      </button>
     <video autoPlay></video>
+    {/*<p>{usersOnline}</p>*/}
   </Fragment>
 );
 
@@ -14,6 +23,18 @@ export default compose(
     video: {
       width: 400,
       height: 300
+    }
+  }),
+  withState('username', 'changeUsername', ''),
+  withState('yourId', 'changeYourId', ''),
+  withHandlers({
+    login: props => event => {
+      socketConnection.emit('join', {
+        username: props.username
+      });
+      socketConnection.on('usersonline', (onlineUsers) => {
+        console.log(onlineUsers);
+      })
     }
   }),
   lifecycle({
